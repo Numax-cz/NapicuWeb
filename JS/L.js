cbb5bad093d49d32d4ef4ab9b338275c73faf147
Numax-cz <marcel.mikolas@gmail.com>
2020-11-12 12:31:43 +0100

    weeeeb!

--- JS/L.js
+++ JS/L.js
@@ -1,3 +1,3 @@
 const xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "https://napicu.cf/JS/pocitadlo.php", false );
+xmlHttp.open( "GET", "https://napicu.eu/JS/pocitadlo.php", false );
 xmlHttp.send( null );
