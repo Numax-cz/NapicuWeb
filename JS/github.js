a64aa88d0e43eb4d006c0237649eb58d435343b8
Marcel Mikolas <marcel.mikolas@gmail.com>
2020-12-12 10:31:25 +0100

    web

--- JS/github.js
+++ JS/github.js
@@ -1,11 +1,24 @@
 var Button = document.querySelector('input[name=darkMod]')
+var Cerna = ('CernaPyco')
+var Bejla = ('BejlaPyco')
+var SusenkyVole =document.cookie.split(';').map(cookie => cookie.split('=')).reduce((L, [key, value]) => ({...L, [key.trim()]: decodeURIComponent(value)}), {})
+var BarvaMode = SusenkyVole.darkmod
+if (BarvaMode == Cerna){
+    document.getElementById('ButtonID').checked = true;
+    document.documentElement.setAttribute('MoreCerna', Cerna) 
+}else{
+    document.getElementById('ButtonID').checked = false;
+    document.documentElement.setAttribute('MoreCerna', Bejla) 
+}
 Button.addEventListener('change', function(){
     if(this.checked){
         HejMorecasMoreAnimaceToToAsiMoznabude()
-        document.documentElement.setAttribute('MoreCerna','CernaPyco')
+        document.documentElement.setAttribute('MoreCerna', Cerna)
+        cookies = document.cookie = `darkmod=${Cerna}`;
     }else{
         HejMorecasMoreAnimaceToToAsiMoznabude()
-        document.documentElement.setAttribute('MoreCerna','BejlaPyco')
+        document.documentElement.setAttribute('MoreCerna', Bejla)
+        cookies = document.cookie = `darkmod=${Bejla}`;
     }
 })
 let HejMorecasMoreAnimaceToToAsiMoznabude = () => {
@@ -14,3 +27,5 @@ let HejMorecasMoreAnimaceToToAsiMoznabude = () => {
         document.documentElement.classList.remove('moresmooooth')
     }, 1000) 
 }
+
+
