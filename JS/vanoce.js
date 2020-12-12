1c139166e7b49bcea059a9516fef24425cf5b266
Marcel Mikolas <marcel.mikolas@gmail.com>
2020-12-12 12:09:13 +0100

    Web

--- JS/vanoce.js
+++ JS/vanoce.js
@@ -1,23 +1,16 @@
 var nevím = new Date('Dec 24, 2020 00:00.00').getTime();
-
-
-    
 function novejrokpico(){
     var čas = new Date().getTime()
         cn = nevím - čas;
-
         var sekundy = 1000;
         var minuty = sekundy * 60;
         var hodiny = minuty * 60;
         var dny = hodiny * 24;
         
-    
-
         var d = Math.floor(cn / (dny));
         var h = Math.floor((cn % (dny)) / (hodiny)) ;
         var m = Math.floor((cn % (hodiny)) / (minuty));
         var s = Math.floor((cn % (minuty)) / sekundy);
-
 
         document.getElementById('Dny').innerText = d; 
         document.getElementById('Hodiny').innerText = h;
@@ -26,7 +19,7 @@ function novejrokpico(){
 }
 setInterval(function(){novejrokpico();},1000)  
 
-// var hudba = document.getElementById('hudba').play();
-// function HratHudbu(){
-//     hudba.play();
-// }
+var hudba = document.getElementById('hudba').play();
+function HratHudbu(){
+    hudba.play();
+}
