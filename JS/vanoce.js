c9dd4733c09c435ed425060e3f93d38ddff10f0d
Marcel Mikolas <marcel.mikolas@gmail.com>
2020-12-23 09:15:37 +0100

    web

--- JS/vanoce.js
+++ JS/vanoce.js
@@ -1,42 +1,22 @@
-var nevím = new Date('Dec 24, 2020 00:00.00').getTime();
-function novejrokpico(){
-    var čas = new Date().getTime()
-        cn = nevím - čas;
-        var sekundy = 1000;
-        var minuty = sekundy * 60;
-        var hodiny = minuty * 60;
-        var dny = hodiny * 24;
-        
-        var d = Math.floor(cn / (dny));
-        var h = Math.floor((cn % (dny)) / (hodiny)) ;
-        var m = Math.floor((cn % (hodiny)) / (minuty));
-        var s = Math.floor((cn % (minuty)) / sekundy);
+var nevim = new Date('Dec 24, 2020 00:00:00').getTime();
+setInterval(function(){
+    var cas = new Date().getTime()
+    cn = nevim - cas;
+    var sekundy = 1000;
+    var minuty = sekundy * 60;
+    var hodiny = minuty * 60;
+    var dny = hodiny * 24;
+            
+    var d = Math.floor(cn / (dny));
+    var h = Math.floor((cn % (dny)) / (hodiny)) ;
+    var m = Math.floor((cn % (hodiny)) / (minuty));
+    var s = Math.floor((cn % (minuty)) / sekundy);
 
-        document.getElementById('Dny').innerText = d; 
-        document.getElementById('Hodiny').innerText = h;
-        document.getElementById('Minuty').innerText = m;
-        document.getElementById('Sekundy').innerText = s;
-
-
-
-       
-}
-setInterval(function(){novejrokpico();},1000)  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.getElementById('Dny').innerHTML = d; 
+    document.getElementById('Hodiny').innerHTML = h;
+    document.getElementById('Minuty').innerHTML = m;
+    document.getElementById('Sekundy').innerHTML = s;
+}, 1000);
 
 
 
