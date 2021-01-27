683a3ec8a0027cd017338910a89c65ff6098caed
Numax-cz¨ <marcel.mikolas@gmail.com>
2021-01-27 21:57:45 +0100

    web

---
+++ PushDisk.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+Des = "D:\\Test\\Test2"
+Dir = os.getcwd()
+
+try:
+    shutil.copytree(Dir, Des)
+    while True:
+        print('Podařilo se převést soubory do: ' + '\033[33m' + Des + '\033[0m')
+        os.system('pause')
+        break
+except FileExistsError:
+    print('\033[31m' + 'Složka již existuje!!!' + '\033[0m')
+    
+
