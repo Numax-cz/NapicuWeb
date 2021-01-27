f982243ee713cdba9e147f6fdaa102bedbaaecb6
Marcel Mikolas <marcel.mikolas@gmail.com>
2021-01-27 14:27:13 +0100

    web

--- Push.py
+++ Push.py
@@ -3,12 +3,9 @@ import os
 print ('\033[31m' + 'Startuji' + '\033[0m')
 print()
 os.system('git add .')
-print ('\033[31m' + 'Startuji' + '\033[0m')
-
 os.system('git commit -m "web"')
-print ('\033[31m' + 'Startuji' + '\033[0m')
-
 os.system('git push')
-print ('\033[31m' + 'Startuji' + '\033[0m')
-
+print()
+print ('\033[31m' + 'Vše bylo provedeno bez problém!' + '\033[0m')
+os.system('pause')
 
