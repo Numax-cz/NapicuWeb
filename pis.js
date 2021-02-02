4ae3d116123dd40b0d4da5eaeb48546f5d068d97
Marcel Mikolas <marcel.mikolas@gmail.com>
2021-02-02 18:48:13 +0100

    web

--- pis.js
+++ pis.js
@@ -1,86 +1,91 @@
-const TextPole = document.getElementById("TextPole")
-ApiSlov = 'https://viem.pinktube.eu/api/slova?pocet=80'
-var PocetSlov = 80
 
+
+
+var PocetSlov = 20
+ApiSlov = `https://viem.pinktube.eu/api/slova?pocet=${PocetSlov}`
 i = 0
+a = 0
+
 pocetchyb = 0
-fetch(ApiSlov)
+function api(){
+    fetch(ApiSlov)
     .then(res => res.text())
     .then(data =>{
         slovo = JSON.parse(data)
         VetaGenerator()
-        
-
-        
     });
+}
+api()
 
 
 function VetaGenerator(){
-   Veta = (slovo.join(" "))
-    
 
-
-    const SpanSet = Veta.split('').map((pismeno) =>{
+    Pozice = 0
+    var TextPole = document.getElementById("TextPole")
+    Veta = (slovo.join(" "))
+    var SpanSet = Veta.split('').map((pismeno) =>{
         const PismenoSpan = document.createElement('span')
         PismenoSpan.innerHTML = pismeno
         TextPole.appendChild(PismenoSpan)
         return PismenoSpan
     })
-
     function Casovac(){
         Timestart = new Date()
         setInterval(function(){
             if((Pozice + 1)!== DelkaVety){
                 TimeSe = Math.floor((new Date() - Timestart) / 1000)
+                document.getElementById('time').innerText = TimeSe + 's'
+                return TimeSe
             }
         },1000)
     }
-
-
     
 
+   
     
-
-    let Pozice = 0
     let zacatek = SpanSet[Pozice]
     zacatek.classList.add('kurzorpico')
     let DelkaVety = Veta.length
 
+    startTime = () => Casovac()
+    
     document.addEventListener('keydown', (e) =>{
-        if((Pozice + 1)!== DelkaVety){
-            Klavesa = e.key
+       var Klavesa = e.key  
 
-
-            if(Klavesa === zacatek.innerText && (Pozice + 1)!== DelkaVety){
+        if(Pozice + 1 !== DelkaVety){
+            if(Klavesa === zacatek.innerText ){
                 zacatek.classList.remove('kurzorpico')
                 zacatek.classList.add('kurzorpicoDodelanomore')
                 Pozice += 1
+            
                 i = 0
                 zacatek = SpanSet[Pozice]
                 zacatek.classList.add('kurzorpico')
             }else{
-                if(e.key !== 'Shift' && e.key !== '(' && e.key !== ')' && e.key !== '/'){
+                if(Klavesa !== 'Shift' && Klavesa !== '(' && Klavesa !== ')' && Klavesa !== '/' && Klavesa !== 'Dead'){
                     zacatek.classList.add('kurvanedelejchybu') 
                     for(i = i; i < 1; i++){
                         pocetchyb +=1
+                        document.getElementById('spatne').innerText =  pocetchyb
                     }
+                }          
+            }
+            if(Pozice > 0 ){
+                for (a = a; a < 1; a++){
+                    startTime()
                 } 
-                      
             }
-            if (Pozice < 1){
-                Casovac()
-            }
-            
-
-            
-            
-        
-            if (DelkaVety == (Pozice + 1)){
-                wpm = Math.floor(PocetSlov / (TimeSe/60))     
+        }else{
+            if(Klavesa == zacatek.innerText){
+                
+                zacatek.classList.remove('kurzorpico')
+                zacatek.classList.add('kurzorpicoDodelanomore')
+                Minuty = TimeSe/60
+                wpm = Math.floor(PocetSlov / Minuty)
                 document.getElementById('cas').innerText = wpm
+                document.getElementById('spatne').innerText =  pocetchyb
+                document.getElementById('time').innerText = Minuty*60 + 's'
             }
         }
     })
-
-
 }
