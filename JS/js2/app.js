19a17364bbd197bb975d50864b03b0f220e2759d
Marcel <marcel.mikolas@gmail.com>
2021-03-26 21:05:31 +0100

    web

--- JS/js2/app.js
+++ JS/js2/app.js
@@ -16,7 +16,7 @@ particlesJS('snow',
   {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 50, //100
         "density": {
           "enable": true,
           "value_area": 800
