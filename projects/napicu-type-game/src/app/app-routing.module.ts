bb90e41aacd9600048a05fc457e3ca9e213d11d6
Marcel <numax.cz@protonmail.ch>
2021-12-07 20:00:51 +0100

    Přidán TypeGame

---
+++ projects/napicu-type-game/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from 'src/app/error404/error404.component';
+import { IndexComponent } from './index/index.component';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: '**', component: Error404Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
