65dbc815feb24b573a736623a61e718ff9ab93da
Marcel <numax.cz@protonmail.ch>
2021-12-24 08:39:22 +0100

    Přidány Vánoce do složky, opraveny Vánoce

---
+++ NapicuVanoce/JS/vanoce.js
@@ -0,0 +1,35 @@
+var y = new Date().getFullYear();
+var nevim = new Date(`Dec 24, ${y} 00:00:00`).getTime();
+
+setInterval(function(){
+    var cas = new Date().getTime()
+    if (cas > nevim) {
+        document.getElementById('txt').innerText = "Veselé vánoce";
+        
+    } else {
+        
+        cn = nevim - cas;
+        var sekundy = 1000;
+        var minuty = sekundy * 60;
+        var hodiny = minuty * 60;
+        var dny = hodiny * 24;
+        var d = Math.floor(cn / (dny));
+        var h = Math.floor((cn % (dny)) / (hodiny)) ;
+        var m = Math.floor((cn % (hodiny)) / (minuty));
+        var s = Math.floor((cn % (minuty)) / sekundy);
+    
+        document.getElementById('Dny').innerHTML = d; 
+        document.getElementById('Hodiny').innerHTML = h;
+        document.getElementById('Minuty').innerHTML = m;
+        document.getElementById('Sekundy').innerHTML = s;
+        document.getElementById('year').innerHTML = y;
+    }
+            
+}, 1000);
+
+
+
+var hudba = document.getElementById('hudba');
+function HratHudbu(){
+    hudba.play();
+}
