5f7ad7a8c55cd0d93ed36d40a0bac33da41ba636
Marcel <numax.cz@protonmail.ch>
2022-02-17 18:24:08 +0100

    Přidán middleware

---
+++ Server/src/validation/middleware.ts
@@ -0,0 +1,29 @@
+import {RequestHandler, Request, Response, NextFunction} from "express";
+import joi, {ValidationErrorItem} from "joi";
+import {ExecException} from "child_process";
+import {HttpStatusCode} from "../interface/HttpStatusCode";
+
+
+
+
+export function middlewareValidation(schema: joi.Schema): RequestHandler {
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+    try {
+      req.body = await schema.validateAsync(req.body, {
+        stripUnknown: true,
+        allowUnknown: true,
+        abortEarly: false
+      })
+      next();
+    } catch (e: any) {
+      const errors: string[] = [];
+
+      e.details.forEach((error: ValidationErrorItem) => {
+        errors.push(error.message)
+      });
+      res.status(HttpStatusCode.badRequest).send({errors: errors})
+    }
+
+  }
+}
