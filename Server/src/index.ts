7835ddc1418ce9b278548a8fc50eccf2c16f8b01
Marcel <numax.cz@protonmail.ch>
2022-02-19 22:00:22 +0100

    Přidány další controllery

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -2,8 +2,14 @@ import 'dotenv/config'
 import {App} from "./App";
 import {PostController} from "./resources/PostRequest/post.controller";
 import {BiosController} from "./resources/Bios/bios.controller";
+import {PopJonanekCounter} from "./entities/PopJonanek/counter";
+import {PopjonanekController} from "./resources/PopJonanek/popjonanek.controller";
 
 
-const i = new App([new PostController(), new BiosController()], Number(process.env.PORT));
+const i = new App([
+  new PostController(),
+  new BiosController(),
+  new PopjonanekController()
+], Number(process.env.PORT));
 
 i.init();
