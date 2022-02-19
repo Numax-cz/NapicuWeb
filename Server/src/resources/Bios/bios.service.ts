5a3c41dde4ebd94d4a45517a6ed96ab97f3d56c1
Marcel <numax.cz@protonmail.ch>
2022-02-19 22:00:10 +0100

    Přidáno error handling

--- Server/src/resources/Bios/bios.service.ts
+++ Server/src/resources/Bios/bios.service.ts
@@ -1,9 +1,17 @@
 import {PostSchema} from "../PostRequest/post.model";
 import {EmailSchema} from "./bios.model";
+import {HttpException} from "../../exceptions/errors";
+import {HttpStatusCode} from "../../interface/HttpStatusCode";
+import joi from "joi";
+import {BiosWaitList} from "../../entities/Bios/waitListDB";
+
 
 
 export class BiosService{
   public async addToWaitList(email: string){
-    return await EmailSchema.validateAsync({email: email});
+    await EmailSchema.validateAsync({email: email}).catch((e: joi.ValidationErrorItem) => {
+      throw new HttpException( 'Invalid email', HttpStatusCode.badRequest);
+    });
+    return await BiosWaitList.insert({email});
   }
 }
