34d7599bb409dd9bb1ac1a8a817faab5154da237
Marcel <numax.cz@protonmail.ch>
2022-02-20 18:58:44 +0100

    Upravena testovací funkce

--- Server/src/resources/PostRequest/post.controller.ts
+++ Server/src/resources/PostRequest/post.controller.ts
@@ -1,10 +1,10 @@
 import {NapicuApiController} from "../../interface/controller";
 import {NextFunction, Request, Response, Router} from "express";
-import {middlewareValidation, middlewareValidationToManyRequests} from "../../validation/middleware";
 import {PostService} from "./post.service";
 import {PostSchema} from "./post.model";
 import {HttpStatusCode} from "../../interface/HttpStatusCode";
 import {HttpException} from "../../exceptions/errors";
+import {HttpResponse} from "../../util/HttpResponse";
 
 
 export class PostController implements  NapicuApiController{
@@ -14,12 +14,12 @@ export class PostController implements  NapicuApiController{
 
 
   constructor() {
-    this.router.post(`${this.path}`, middlewareValidationToManyRequests(), this.post);
+    this.router.post(`${this.path}`, (req: Request, res: Response, next: NextFunction) => {next()}, this.post);
   }
 
   protected async post(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
     const post = await new PostService().post(req.body.title, req.body.body);
 
-    res.status(HttpStatusCode.created).json(post);
+    res.status(HttpStatusCode.created).json(new HttpResponse(HttpStatusCode.created, true, post));
   }
 }
