03660172a79f37677ee72f2b8a04b9b66206f7ce
Marcel <numax.cz@protonmail.ch>
2022-02-20 18:56:22 +0100

    Odstraněn get request, upraven post request

--- Server/src/resources/PopJonanek/popjonanek.controller.ts
+++ Server/src/resources/PopJonanek/popjonanek.controller.ts
@@ -1,6 +1,6 @@
 import {NapicuApiController} from "../../interface/controller";
 import {NextFunction, Request, Response, Router} from "express";
-import {middlewareValidation} from "../../validation/middleware";
+import {middlewareValidation, middlewareValidationToManyRequests} from "../../validation/middleware";
 import {EmailSchema} from "../Bios/bios.model";
 import {BiosService} from "../Bios/bios.service";
 import {HttpStatusCode} from "../../interface/HttpStatusCode";
@@ -8,35 +8,18 @@ import {PopjonanekService} from "./popjonanek.service";
 import {PopJonanekCounterSchema} from "./popjonanek.model";
 import {PostService} from "../PostRequest/post.service";
 import rateLimit from 'express-rate-limit'
+import {HttpResponse} from "../../util/HttpResponse";
+import {NapicuAPIBasicPOSTResponse, PopJonanekGETApiResponse} from "../../interface/Responses";
 
 export class PopjonanekController implements NapicuApiController{
   public path: string = "/popjonanek";
   public router: Router = Router();
 
   constructor() {
-    this.router.get(`${this.path}/counter`, this.get);
-    this.router.post(`${this.path}/counter`, middlewareValidation(PopJonanekCounterSchema), this.post);
+    this.router.post(`${this.path}/counter`, [middlewareValidation(PopJonanekCounterSchema), middlewareValidationToManyRequests(230)], this.post);
   }
 
-
-  protected async get(req: Request, res: Response, next: NextFunction ): Promise<Response | void> {
-    try {
-      const user = await new PopjonanekService().getCount();
-      res.status(HttpStatusCode.created).json({count: user});
-    }catch (e){
-      //TODO Internal Error;
-    }
+  protected async post(req: Request, res: Response, next: NextFunction): Promise<HttpResponse<NapicuAPIBasicPOSTResponse>>{
+      return await new PopjonanekService().addCount(req.body.clicks);
   }
-
-  protected async post(req: Request, res: Response, next: NextFunction): Promise<Response | void>{
-    try {
-      let {counter} = req.body;
-      const count = await new PopjonanekService().addCount(counter);
-      res.status(HttpStatusCode.created).json(count)
-    }catch (e){
-      //TODO
-    }
-  }
-
-
 }
