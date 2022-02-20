af2ba03f4c37a4ec97f82fd59a3883ca0a18b8a6
Marcel <numax.cz@protonmail.ch>
2022-02-20 18:58:48 +0100

    Upravena testovací funkce

--- Server/src/resources/PostRequest/post.service.ts
+++ Server/src/resources/PostRequest/post.service.ts
@@ -4,6 +4,5 @@ import {PostSchema} from "./post.model";
 export class PostService{
   public async post(title: string, body: string): Promise<any>{
     return await PostSchema.validateAsync({title: title, body: body});
-
   }
 }
