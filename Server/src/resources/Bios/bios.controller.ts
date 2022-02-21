72d4b7e860f03a0a4ba65e2708450c794ce0477f
Marcel <numax.cz@protonmail.ch>
2022-02-21 21:49:07 +0100

    Předěláno vše do configu

--- Server/src/resources/Bios/bios.controller.ts
+++ Server/src/resources/Bios/bios.controller.ts
@@ -5,17 +5,17 @@ import {
   middlewareValidationToManyRequests
 } from "../../validation/middleware";
 import {EmailSchema} from "./bios.model";
-import {PostService} from "../PostRequest/post.service";
-import {HttpStatusCode} from "../../interface/HttpStatusCode";
-import {HttpException} from "../../exceptions/errors";
+
 import {BiosService} from "./bios.service";
+import {api_bios_main_path, api_bios_wait_list} from "../../config/serverConfig";
 
 export class BiosController implements NapicuApiController{
-  public path: string = "/bios";
+  public path: string = api_bios_main_path;
   public router: Router =  Router();
 
   constructor() {
-    this.router.post(`${this.path}/waitlist`, [middlewareValidation(EmailSchema), middlewareValidationToManyRequests(15)], this.post);
+    this.router.post(`${this.path}${api_bios_wait_list}`, [middlewareValidation(EmailSchema), middlewareValidationToManyRequests(15)], this.post);
+    console.log(`${this.path}${api_bios_wait_list}`);
   }
 
 
