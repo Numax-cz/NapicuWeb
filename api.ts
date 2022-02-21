647c886c421a3858ad601f5e8db3b4a159e7b4df
Marcel <numax.cz@protonmail.ch>
2022-02-21 21:46:26 +0100

    Přidán config API

--- api.ts
+++ api.ts
@@ -1,2 +1,9 @@
 // https://viem.pinktube.eu/api/slova?pocet=num
+import {api_bios_main_path, api_bios_wait_list, api_path} from "./Server/src/config/serverConfig";
+
 export const WordsAPI: string = 'https://viem.napicu.eu/api/slova';
+
+const mainApiPath: string = "https://api.napicu.eu"
+
+
+export const biosEmailAPI: string = `${mainApiPath}${api_path}${api_bios_main_path}${api_bios_wait_list}`
