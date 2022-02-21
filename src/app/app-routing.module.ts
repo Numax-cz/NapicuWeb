ffc00e3891808e87021cdb10878b0733fcf6c55b
Marcel <numax.cz@protonmail.ch>
2022-02-21 21:49:19 +0100

    Přidán path pro BIOS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './error404/error404.component';
 import { IndexComponent } from './index/index.component';
+import {BiosWaitListComponent} from "./bios-wait-list/bios-wait-list.component";
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'bios', component: BiosWaitListComponent },
   { path: '**', component: Error404Component },
 ];
 
