ba5550ddd4c7dacb4b00ff77fdc71d06a8439b70
Marcel <numax.cz@protonmail.ch>
2022-02-21 21:48:43 +0100

    Přidán interface

--- Server/src/resources/Bios/bios.model.ts
+++ Server/src/resources/Bios/bios.model.ts
@@ -1,6 +1,7 @@
 import joi from "joi";
+import {BiosWaitListPOSTApiModel} from "../../interface/Model";
 
 
-export const EmailSchema = joi.object({
+export const EmailSchema = joi.object<BiosWaitListPOSTApiModel>({
   email: joi.string().required().email(),
 });
