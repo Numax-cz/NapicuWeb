b2eb227aabfe63dd87eba016606bc9282afab686
Marcel <numax.cz@protonmail.ch>
2022-06-05 19:24:58 +0200

    Přidán config, upraveno NapicuPocasi

--- projects/napicu-pocasi/src/app/app.component.ts
+++ projects/napicu-pocasi/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NapicuPocasiCities, NapicuPocasiCitiesMaxView} from "./config";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public declare inputValue: string;
+  public filterList: string[] = [];
+
 
   public onInputChange(event: Event): void {
-
+    this.updateFilterMenu();
   };
+
+  public updateFilterMenu(): void {
+    this.filterList = NapicuPocasiCities.filter((item: string) => {
+      // return this.inputValue.toLowerCase().includes(item.toLowerCase());
+      return item.toLocaleLowerCase().startsWith(this.inputValue.toLocaleLowerCase())
+    });
+    this.filterList.length = NapicuPocasiCitiesMaxView;
+  }
+
+
 }
