b6dc87daea15352752579406d71da944445a72e9
Marcel <numax.cz@protonmail.ch>
2022-06-13 18:03:18 +0200

    Přidán interface Apíček

---
+++ open-api/api/api.ts
@@ -0,0 +1,9 @@
+export * from './napicuIpController.service';
+import { NapicuIpControllerService } from './napicuIpController.service';
+export * from './napicuPocasiController.service';
+import { NapicuPocasiControllerService } from './napicuPocasiController.service';
+export * from './napicuPopJonanekController.service';
+import { NapicuPopJonanekControllerService } from './napicuPopJonanekController.service';
+export * from './napicuTypeGameController.service';
+import { NapicuTypeGameControllerService } from './napicuTypeGameController.service';
+export const APIS = [NapicuIpControllerService, NapicuPocasiControllerService, NapicuPopJonanekControllerService, NapicuTypeGameControllerService];
---
+++ open-api/model/models.ts
@@ -0,0 +1,5 @@
+export * from './napicuIPResponseModel';
+export * from './napicuPocasiResponseModel';
+export * from './napicuPopJonanekPostModel';
+export * from './napicuPopJonanekResponseModel';
+export * from './requestExceptionSchema';
