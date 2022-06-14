1dbb44d19cd40c4a9d787b17149bccd4634c257d
Marcel <numax.cz@protonmail.ch>
2022-06-14 17:14:45 +0200

    Smazány nepotřebné componenty

--- api.ts
+++ api.ts
@@ -2,16 +2,4 @@
 import {api_bios_main_path, api_bios_wait_list, api_path} from "./Server/src/config/serverConfig";
 const mainApiPath: string = "https://api.napicu.eu";
 
-
 export const biosEmailAPI: string = `${mainApiPath}${api_path}${api_bios_main_path}${api_bios_wait_list}`;
-
-
-
-
-
-//TODO Move here
-export const NapicuApiURLWords: string = 'http://localhost:8080/words';
-
-export const NapicuApiURLWeather: string = 'http://localhost:8080/weather';
-
-export const NapicuApiURLIp: string = "http://localhost:8080/ip"; //TODO
